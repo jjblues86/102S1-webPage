7bb5f3bfdec83dbdca8ac4aa33afbea3e4d76f22
jjblues86 <jeromejoof@yahoo.com>
2019-10-29 22:09:22 -0700

    added validation

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
 
 //Prompt window
 var name = prompt("Please enter your name  ", "");
+name = name.toUpperCase();
+// name = String();
 
 if(name != null) {
   document.getElementById("welcome").innerHTML = "Hello " + name + " How is it going?";
-}
+ } else {
+  document.getElementById("not").innerHTML = "Please enter your name!";
+ }
 
 
 //Confirm window
@@ -14,10 +18,24 @@ function response() {
 }
 
 //Math random
-
-// var random = document.getElementById('math').innerHTML;
 function random(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if(hourNow > 18) {
+  greeting = 'Good evening!';
+} else if (hourNow > 12) {
+  greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+  greeting = 'Good morning!';
+} else {
+  greeting = 'Welcome';
+}
+
+document.write('<h3>' + greeting + '</h3>');
 
 
